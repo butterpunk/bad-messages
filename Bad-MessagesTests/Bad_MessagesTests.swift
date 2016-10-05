976c85a86fa3fea7cf19b7b3bbf12d13de632d88
Blake Butterworth <blakebutterworth@Blakes-Mac-mini.local>
2016-10-05 09:24:19 -0500

    first commit

---
+++ Bad-MessagesTests/Bad_MessagesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bad_MessagesTests.swift
+//  Bad-MessagesTests
+//
+//  Created by Blake Butterworth on 9/26/16.
+//  Copyright © 2016 Blake. All rights reserved.
+//
+
+import XCTest
+@testable import Bad_Messages
+
+class Bad_MessagesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'Bad-Messages' do
+	pod 'Pushwoosh'
+	pod 'Alamofire'
+end
+
+target 'Bad-MessagesTests' do
+
+end
+
+target 'Bad-MessagesUITests' do
+
+end
+
